37730559651321d1ef62e98b1c4798f6c0e7d933
james-1231 <144109277+james-1231@users.noreply.github.com>
2023-09-12 08:58:50 +0000

    age question

---
+++ James.py
@@ -0,0 +1,7 @@
+print('hello world')
+ptint('what is you name')
+my Name=input()
+print('it is good to meet you,'+my Name)
+print(len(my Name))
+Myage=input
+print('you will be'+str(int(Myage)+1)+'in one year.')
